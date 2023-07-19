64ec5ff3d3341516f3b1565dd72f1051e65a72dd
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-07-19 11:38:50 +0100

    Defining the CRUD endpoint: Create, List, Get, Update, Delete for user & tweet

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import userRoutes from './routes/userRoutes';
+import tweetRoutes from './routes/tweetRoutes';
 
 const app = express();
 app.use(express.json());
+app.use('/user', userRoutes);
+app.use('/tweet', tweetRoutes);
 
 // app.METHOD(PATH, HANDLER)
-
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
+
+
 
 app.listen(3001, () => {
     console.log("Server ready at localhost:3001");
