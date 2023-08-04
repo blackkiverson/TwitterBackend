9ff0f80a03cbf9dfd2c675b8b3c014ad207143ea
SamuelOI <95894328+blackkiverson@users.noreply.github.com>
2023-08-04 10:40:36 +0100

    no comment needed

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,30 +1,41 @@
 import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = Router();
+const prisma = new PrismaClient();
 
 // User CRUD
 // creating a user or data we use post request
 
 // Create user
-router.post('/', (req, res) => {
-    res.status(501).json({
-        error: 'Not Implemented'
+router.post('/', async (req, res) => {
+    const { email, name, username } = req.body;
+    console.log(email, name, username);
+    const result = await prisma.user.create({
+        data: {
+            email,
+            name,
+            username,
+            bio: "Hello I am new on twitter",
+        },
     });
+
+    res.json(result);
 });
 
 // list users
-router.get('/', (req, res) => {
-    res.status(501).json({
-        error: 'Not Implemented'
-    });
+router.get('/', async (req, res) => {
+    const allUser = await prisma.user.findMany();
+
+    res.json(allUser);
 });
 
 // get one user
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
     const {id} = req.params;
-    res.status(501).json({
-        error: 'Not Implemented: ${id}'
-    });
+    const user = await prisma.user.findUnique({ where: {id: Number(id)} });
+
+    res.json(user);
 });
 
 // update user
